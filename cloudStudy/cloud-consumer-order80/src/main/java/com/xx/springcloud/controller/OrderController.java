64d034afebe743c579d23b55c8cffe367c474e48
codeXu <xl530522@163.com>
2022-06-03 10:44:18 +0800

    Ribbon核心组件IRule,将默认的轮询配置为随机访问

--- cloudStudy/cloud-consumer-order80/src/main/java/com/xx/springcloud/controller/OrderController.java
+++ cloudStudy/cloud-consumer-order80/src/main/java/com/xx/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.xx.springcloud.controller;
 import com.xx.springcloud.entities.CommonResult;
 import com.xx.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,15 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
+
+    @GetMapping(value = "consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            return forEntity.getBody();
+        } else {
+            return new CommonResult(444, "操作失败");
+        }
+    }
+
 }
