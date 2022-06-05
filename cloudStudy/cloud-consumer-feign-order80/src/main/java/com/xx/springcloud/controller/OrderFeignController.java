01d9960e48fc22711f94aa9b365c0befec70791d
codeXu <xl530522@163.com>
2022-06-05 11:30:20 +0800

    添加OpenFeign

---
+++ cloudStudy/cloud-consumer-feign-order80/src/main/java/com/xx/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,24 @@
+package com.xx.springcloud.controller;
+
+import com.xx.springcloud.entities.CommonResult;
+import com.xx.springcloud.entities.Payment;
+import com.xx.springcloud.service.PaymentFeignService;
+import feign.Param;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
