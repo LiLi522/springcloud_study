f834b0493b3995995ff8f64ba818914007a1403d
codeXu <xl530522@163.com>
2022-06-06 14:42:37 +0800

    构建高并发测试，暴露出问题

---
+++ cloudStudy/cloud-provider-hystrix-payment8001/src/main/java/com/xx/springcloud/service/PaymentService.java
@@ -0,0 +1,27 @@
+package com.xx.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+
+    /*
+    正常访问
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "paymentInfo_OK，id" + id + "\t" + "O(∩_∩)O";
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+        //暂停几秒钟线程
+        int timeNumber = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池：" + Thread.currentThread().getName() + "paymentInfo_TimeOut, id" + id + "\t" + "耗时" + timeNumber + "秒钟";
+    }
+}
