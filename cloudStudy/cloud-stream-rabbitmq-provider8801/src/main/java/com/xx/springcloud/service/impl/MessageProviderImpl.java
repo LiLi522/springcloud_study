dd1c25683167e5c633eb8ade15703d231807da37
codeXu <xl530522@163.com>
2022-06-08 12:52:42 +0800

    Stream消息驱动之消费者

--- cloudStudy/cloud-stream-rabbitmq-provider8801/src/main/java/com/xx/springcloud/service/impl/MessageProviderImpl.java
+++ cloudStudy/cloud-stream-rabbitmq-provider8801/src/main/java/com/xx/springcloud/service/impl/MessageProviderImpl.java
@@ -1,6 +1,14 @@
 package com.xx.springcloud.service.impl;
 
-import cn.hutool.core.lang.UUID;
+/**
+ * 不能使用hutool工具包下的UUID方法 不然接收到的消息显示有问题
+ * 消费者1号，---------》接受到的消息：{"leastSignificantBits":-8832270078250038563,"mostSignificantBits":4123372299366319625}	    port：8802
+ * 消费者1号，---------》接受到的消息：{"leastSignificantBits":-8407276894973599148,"mostSignificantBits":846621491713034556}	    port：8802
+ * 消费者1号，---------》接受到的消息：{"leastSignificantBits":-5398574222103338134,"mostSignificantBits":1273655466196355635}	    port：8802
+ * 消费者1号，---------》接受到的消息：{"leastSignificantBits":-7826444096809963226,"mostSignificantBits":1899250736983328135}	    port：8802
+ * 消费者1号，---------》接受到的消息：{"leastSignificantBits":-8020015136426811091,"mostSignificantBits":-8952782286226568171}	    port：8802
+ * **/
+//import cn.hutool.core.lang.UUID;
 import com.xx.springcloud.service.IMessageProvider;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
@@ -8,6 +16,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 
 @EnableBinding(Source.class)    //定义消息的推送管道
