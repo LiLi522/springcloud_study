450fbeccd4714cefca72a09b981f2d3b8fdd33d9
codeXu <xl530522@163.com>
2022-06-09 17:01:31 +0800

    Sentinel中客户自定义限流处理逻辑

--- cloudStudy/cloudalibaba-sentinel-service8401/src/main/java/com/xx/springcloud/controller/RateLimitController.java
+++ cloudStudy/cloudalibaba-sentinel-service8401/src/main/java/com/xx/springcloud/controller/RateLimitController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.xx.springcloud.entities.CommonResult;
 import com.xx.springcloud.entities.Payment;
+import com.xx.springcloud.myhandler.CustomerBlockHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,18 @@ public class RateLimitController {
     }
 
     @GetMapping(value = "/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
     public CommonResult byUrl() {
         return new CommonResult(200, "按Url限流测试OK", new Payment(2022L, "serial2023"));
+    }
+
+
+    //CustomerBlockHandler
+    @GetMapping(value = "/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200, "按客户自定义限流测试OK", new Payment(2022L, "serial2024"));
     }
 }
