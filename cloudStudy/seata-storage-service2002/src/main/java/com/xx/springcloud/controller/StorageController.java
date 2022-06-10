b78e105f8f2c0f26179ce10f6a10ef918759dfed
codeXu <xl530522@163.com>
2022-06-10 18:54:31 +0800

    请求Storage时的Bug修复

--- cloudStudy/seata-storage-service2002/src/main/java/com/xx/springcloud/controller/StorageController.java
+++ cloudStudy/seata-storage-service2002/src/main/java/com/xx/springcloud/controller/StorageController.java
@@ -5,6 +5,7 @@ import com.xx.springcloud.domain.CommonResult;
 import com.xx.springcloud.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,7 @@ public class StorageController {
     /**
      * 扣减库存
      * **/
-    @GetMapping(value = "storage/decrease")
+    @RequestMapping(value = "/storage/decrease")
     public CommonResult decrease(Long productId, Integer count) {
         storageService.decrease(productId, count);
         return new CommonResult(200, "扣减库存成功！！！");
